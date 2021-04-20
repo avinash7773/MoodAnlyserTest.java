7fc96178999c78298721c5e2ccc468fcde3da46f
avinash7773 <javinash228@gmail.com>
2021-04-20 12:40:24 +0530

    Inform user if user provide invalid mood

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -3,23 +3,28 @@ package com.bridgelabz;
 public class MoodAnalyser {
     String message;
 
+    public MoodAnalyser() {
+
+    }
+
     public MoodAnalyser(String message) {
         this.message = message;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MoodAnalysisException {
         MoodAnalyser moodanalyser = new MoodAnalyser(null);
         moodanalyser.analyseMood();
     }
 
-    public String analyseMood() {
+    //Handle nullpointerException
+    public String analyseMood() throws MoodAnalysisException {
         try {
             if (message.contains("sad"))
                 return "sad";
             else
                 return "Happy";
         } catch (NullPointerException e) {
-            return "Happy";
+            throw new MoodAnalysisException("Please enter valid mood");
         }
 
     }
--- src/test/java/com/bridgelabz/MoodAnalyseTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyseTest.java
@@ -2,14 +2,22 @@ package com.bridgelabz;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
 
 public class MoodAnalyseTest {
 
     @Test
     void testGivenNullMood() {
         MoodAnalyser moodAnalyser = new MoodAnalyser(null);
-        String mood = moodAnalyser.analyseMood();
-        Assert.assertEquals("Happy",mood);
+        String mood = null;
+        try {
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(MoodAnalysisException.class);
+            mood = moodAnalyser.analyseMood();
+            Assert.assertEquals("Happy",mood);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
 
     }
 }
